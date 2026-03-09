d8269c9f5a09e72ea7bbb8f20cefc25b1033f3a9
ferynurwahid-cyber <ferynurwahid@gmail.com>
2026-03-10 03:11:01 +0700

    js/supabase.js

--- js/supabaseClient.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
-
-const supabaseUrl = "https://fzplvluohnbuggvdqkkw.supabase.co"
-
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6cGx2bHVvaG5idWdndmRxa2t3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5OTE4NTMsImV4cCI6MjA4ODU2Nzg1M30.Q3BDZRWJhai63P_yWAbt-TTNqqtnS45zjfevDHbIj1U"
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
