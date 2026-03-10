87b01a022806fe7e1df64704a83ae5bc84ec5c50
ferynurwahid-cyber <ferynurwahid@gmail.com>
2026-03-10 16:22:42 +0700

    Delete js/js/js directory

--- js/js/js/supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm"
-
-export const supabase = createClient(
-"https://fzplvluohnbuggvdqkkw.supabase.co",
-"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6cGx2bHVvaG5idWdndmRxa2t3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI5OTE4NTMsImV4cCI6MjA4ODU2Nzg1M30.Q3BDZRWJhai63P_yWAbt-TTNqqtnS45zjfevDHbIj1U"
-)
