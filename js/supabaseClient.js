3d2c0165706892ae8ff427af73244a7bce661e5a
ferynurwahid-cyber <ferynurwahid@gmail.com>
2026-03-12 23:51:37 +0700

    supabaseClient.js

---
+++ js/supabaseClient.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://fzplvluohnbuggvdqkkw.supabase.co"
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9..."
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+)
